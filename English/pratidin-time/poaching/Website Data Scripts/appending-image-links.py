b1b50395c1827472ddf3b5cb0bc4bcd7f701c456
lovenya <lovenya2002@gmail.com>
2024-07-24 01:09:42 +0530

    1.6 gb upload

---
+++ English/pratidin-time/poaching/Website Data Scripts/appending-image-links.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Specify the input file path
+input_file_path = r"D:\AI-ML\Data-Scraping-Wildlife-care\English\pratidin-time\poaching\Data Excels (Main Data, Shortened Excel)\pratidin-time-assamese-poaching-cleaned.xlsx"
+
+# Specify the output file path
+output_file_path = r"D:\AI-ML\Data-Scraping-Wildlife-care\English\pratidin-time\poaching\Data Excels (Main Data, Shortened Excel)\pratidin-time-assamese-poaching-cleaned-sanitized.xlsx"
+
+# Load the Excel file
+df = pd.read_excel(input_file_path)
+
+# Update the 'article-post-image-src' column
+df["article-post-image-src"] = df["article-post-image-src"].apply(
+    lambda x: "https:" + x if pd.notna(x) else x
+)
+
+# Save the updated DataFrame to a new Excel file
+df.to_excel(output_file_path, index=False)
+
+# Print completion message
+print(f"File has been successfully sanitized and saved to {output_file_path}")
