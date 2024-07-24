8f8f9a884cc261deddd3c54b8bd5f736c5f4fb0a
lovenya <lovenya2002@gmail.com>
2024-07-24 14:59:02 +0530

    punjabi-jagran done

--- Punjabi/punjabi-jagran/wildlife/Website Data Scripts/website-data-download.py
+++ Punjabi/punjabi-jagran/wildlife/Website Data Scripts/website-data-download.py
@@ -4,12 +4,12 @@ import pandas as pd
 from urllib.parse import urlparse, parse_qs
 
 # Load the data from the Excel file
-file_path = r"path-to-shortened-excel"
+file_path = r"D:\AI-ML\Data-Scraping-Wildlife-care\Punjabi\punjabi-jagran\wildlife\Data Excels (Main Data, Shortened Excel)\punjabi-jagran-wildlife-shortened.xlsx"
 data = pd.read_excel(file_path)
 
 # Create directories if they don't exist
-text_folder = "website-text-files"
-image_folder = "website-images"
+text_folder = "punjabi-jagran-wildlife-text-files"
+image_folder = "punjabi-jagran-wildlife-images"
 os.makedirs(text_folder, exist_ok=True)
 os.makedirs(image_folder, exist_ok=True)
 
